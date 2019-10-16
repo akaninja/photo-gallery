db6aa7146ce7f1447a20944cf4b8087ec7ea28bf
André Kanamura <kanamura@gmail.com>
2019-10-16 08:59:30 -0300

    WIP user views albums list

--- spec/feature/user_view_album_list_spec.rb
+++ spec/feature/user_view_album_list_spec.rb
@@ -1,14 +1,29 @@
 require 'rails_helper'
 
-feature 'User views his album list' do
+feature 'User views his albums list' do
   scenario 'successfully' do
     Album.create!(title: 'Hepdogs')
     Album.create!(title: 'Hepcats')
 
     visit root_path
     click_on 'Meus álbuns'
-    
+
     expect(page).to have_content('Hepdogs')
     expect(page).to have_content('Hepcats')
+  end
+
+  scenario 'and views especific album' do
+
+    album = Album.create!(title: 'Hepdogs')
+    album.photos.attach(io: File.open(Rails.root
+         .join('spec', 'support', 'hepcats.jpg')), filename: 'hepcats.jpg')
+    Album.create!(title: 'Hepcats')
+
+    visit root_path
+    click_on 'Meus álbuns'
+    click_on 'Hepdogs'
+
+    expect(page).to have_content('Hepdogs')
+    expect(page).to have_css('img[src*="hepcats.jpg"]')
   end
 end
